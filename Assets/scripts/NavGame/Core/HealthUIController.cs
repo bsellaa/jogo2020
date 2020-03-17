4e21941539091f536007be0edf720bf772177bdb
bsellaa <marcella.basilio.souza@gmail.com>
2020-03-17 14:20:43 -0400

    Health events

--- Assets/scripts/NavGame/Core/HealthUIController.cs
+++ Assets/scripts/NavGame/Core/HealthUIController.cs
@@ -2,30 +2,40 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 namespace NavGame.Core
 {
     [RequireComponent(typeof(DamageableGameObject))]
+
     public class HealthUIController : MonoBehaviour
     {
         public GameObject healthUIPrefab;
         public Transform healthPosition;
         GameObject healthUI;
         Transform cam;
+        DamageableGameObject damageable;
+        Image healthSlider;
+
         void Awake()
         {
             Canvas canvas = FindWorldCanvas();
-            if (canvas == null)
+            if(canvas == null)
             {
                 throw new Exception("A WorldSpace canvas was needed");
             }
             cam = Camera.main.transform;
             healthUI = Instantiate(healthUIPrefab, canvas.transform);
+            healthSlider = healthUI.transform.GetChild(0).GetComponent<Image>();
+            damageable = GetComponent<DamageableGameObject>();
+
+            damageable.onHealthChanged += UpdateHealth;
+            damageable.onDied += DestroyHealth;
         }
 
         void LateUpdate()
         {
-            if (healthUI != null)
+            if(healthUI != null)
             {
                 healthUI.transform.position = healthPosition.position;
                 healthUI.transform.forward = -cam.forward;
@@ -36,7 +46,7 @@ namespace NavGame.Core
         {
             foreach (Canvas c in FindObjectsOfType<Canvas>())
             {
-                if (c.renderMode == RenderMode.WorldSpace)
+                if(c.renderMode == RenderMode.WorldSpace)
                 {
                     return c;
                 }
@@ -44,6 +54,18 @@ namespace NavGame.Core
             return null;
         }
 
-    }
+        void UpdateHealth(int maxHealth, int currentHealth)
+        {
+            if(healthUI != null)
+            {
+                float healthPercent = (float) currentHealth / maxHealth;
+                healthSlider.fillAmount = healthPercent;
+            }
+        }
 
+        void DestroyHealth()
+        {
+            Destroy(healthUI);
+        }
+    }
 }
