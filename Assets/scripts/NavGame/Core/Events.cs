a4309836989eb8f37409b86b29e9773b59d129a6
bsellaa <marcella.basilio.souza@gmail.com>
2020-03-17 11:28:07 -0400

    HealthUI prefab, health changed an died events

--- Assets/scripts/NavGame/Core/Events.cs
+++ Assets/scripts/NavGame/Core/Events.cs
@@ -4,4 +4,6 @@ namespace NavGame.Core
 {
     // CHARACTER EVENTS
     public delegate void OnAttackHitEvent(Vector3 position);
+    public delegate void OnHealthChangedEvent(int maxHealth, int currentHealt);
+    public delegate void OnDiedEvent();
 }
